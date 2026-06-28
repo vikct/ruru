2de781eff22c629151558d5dd3499867185469c7
vikct <victor.tan@rentguard.com.my>
2026-06-28 16:14:00 +0800

    feat: persist desktop sidebar collapsed state in localStorage

--- angular/src/app/core/layout/layout.component.ts
+++ angular/src/app/core/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppSidebarComponent } from './sidebar/sidebar.component';
 import { AppHeaderComponent } from './header/header.component';
@@ -11,11 +11,19 @@ import { AppHeaderComponent } from './header/header.component';
   styleUrl: './layout.component.scss'
 })
 export class AppLayoutComponent {
-  readonly collapsed = signal(false);
+  readonly collapsed = signal<boolean>(
+    localStorage.getItem('ruru-sidebar-collapsed') === 'true'
+  );
   readonly isMobileOpen = signal(false);
 
   readonly containerClass = computed(() => ({
     'layout-static-inactive': this.collapsed(),
     'layout-mobile-active': this.isMobileOpen()
   }));
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem('ruru-sidebar-collapsed', String(this.collapsed()));
+    });
+  }
 }
